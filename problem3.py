711207a49a724ae7e08df8d1eacdc045ce687b67
fboyle2001 <finlayboyle2001@gmail.com>
2021-01-17 14:46:44 +0000

    Implemented bilateral and made swirl

--- problem3.py
+++ problem3.py
@@ -40,6 +40,43 @@ def apply_colour_curve_hsv(hsvimg, map):
         for y in range(0, img_height):
             hsvimg[x, y][1] = map[hsvimg[x, y][1]]
 
+def bilateral_filter(img, size, sigma_colour, sigma_space):
+    new_img = np.zeros(img.shape, np.uint8)
+    img_height, img_width, _ = new_img.shape
+    gaussian = lambda input, sigma: (1 / (sigma * math.sqrt(2 * math.pi))) * math.exp(-(input ** 2) / (2 * sigma ** 2))
+
+    for x in range(0, img_width):
+        for y in range(0, img_height):
+            numer_b, numer_g, numer_r = 0, 0, 0
+            denom_b, denom_g, denom_r = 0, 0, 0
+            b, g, r = img[x, y]
+
+            for n_x in range(max(x - size, 0), min(x + size + 1, img_width - 1)):
+                for n_y in range(max(y - size, 0), min(y + size + 1, img_height - 1)):
+                    nb, ng, nr = img[n_x, n_y].astype(dtype=int)
+
+                    g_1 = gaussian((x - n_x) ** 2 + (y - n_y) ** 2, sigma_space)
+                    g_2_b = gaussian(b - nb, sigma_colour)
+                    g_2_g = gaussian(g - ng, sigma_colour)
+                    g_2_r = gaussian(r - nr, sigma_colour)
+
+                    numer_b += g_1 * g_2_b * nb
+                    numer_g += g_1 * g_2_g * ng
+                    numer_r += g_1 * g_2_r * nr
+
+                    denom_b += g_1 * g_2_b
+                    denom_g += g_1 * g_2_g
+                    denom_r += g_1 * g_2_r
+
+            # if numer_b > denom_b or numer_g > denom_g or numer_r > denom_r:
+            #     print("?")
+
+            new_img[x, y][0] = min(255, numer_b / denom_b)
+            new_img[x, y][1] = min(255, numer_g / denom_g)
+            new_img[x, y][2] = min(255, numer_r / denom_r)
+
+    return new_img
+
 #print(np.log([]))
 
 sf = 255 / math.log(255) * 0.75
@@ -54,15 +91,19 @@ map = np.concatenate(([0], map))
 #print(map)
 
 
-# import sys
-# sys.exit(0)
 
 img = cv2.imread("face2.jpg", cv2.IMREAD_COLOR)
-filtered = cv2.bilateralFilter(img, 5, 40, 40)
+#filtered = cv2.bilateralFilter(img, 5, 40, 40)
+filtered = bilateral_filter(img, 2, 15, 15)
+#my_filtered = np.zeros(img.shape, np.uint8)
+
+#concat = np.concatenate((img, filtered, my_filtered), axis=1)
+
+#filtered = cv2.blur(img, (2, 2))
 hsvf = cv2.cvtColor(filtered, cv2.COLOR_BGR2HSV)
 #apply_colour_curve(hsvf, map)
-apply_colour_curve_hsv(hsvf, map)
-#apply_colour_hsv(hsvf, 1.2)
+#apply_colour_curve_hsv(hsvf, map)
+apply_colour_hsv(hsvf, 1.5)
 rgbf = cv2.cvtColor(hsvf, cv2.COLOR_HSV2BGR)
 
 concat = np.concatenate((img, rgbf), axis=1)
--- problem4.py
+++ problem4.py
@@ -5,57 +5,53 @@ import math
 
 img = cv2.imread("face2.jpg", cv2.IMREAD_COLOR)
 
-def to_polar_coordinates(x, y):
-    if x == 0 and y == 0:
-        return None
+s = set()
 
-    theta = math.atan2(y, x)
-    r = math.sqrt(x ** 2 + y ** 2)
-
-    return (r, theta)
-
-def to_cartesian(r, theta):
-    return (r * math.cos(theta), r * math.sin(theta))
-
-def swirl_image(img):
+def swirl_image(img, swirl_radius, swirl_angle, swirl_direction=-1):
+    # Create an empty image of the same dimensions
     new_img = np.zeros(img.shape, np.uint8)
 
+    # Calculate the center of the image
     img_height, img_width, _ = img.shape
     centre_x, centre_y = img_height // 2, img_width // 2
-    sr = 100
-    st = 1
 
+    # Loop over each pixel
     for x in range(0, img_width):
         for y in range(0, img_height):
-            px = x - centre_x
-            py = y - centre_y
-            pd = math.sqrt(px ** 2 + py ** 2)
-            pa = math.atan2(py, px)
-            sa = 1 - (pd / sr)
+            # Center the pixel's coordinates
+            centred_x = x - centre_x
+            centred_y = y - centre_y
 
-            if sa > 0:
-                ta = st * sa * math.pi * 2
-                pa += ta
-                px = round(math.cos(pa) * pd)
-                py = round(math.sin(pa) * pd)
+            # Convert to polar coordinates
+            # atan2 handles quadrants
+            r = math.sqrt(centred_x ** 2 + centred_y ** 2)
+            theta = math.atan2(centred_y, centred_x)
 
-                new_img[x, y] = img[px + centre_x, py + centre_y]
-            else:
-                new_img[x, y] = img[x, y]
+            # Idea is that the further from the origin, the less we want to rotate the pixels
+            # this will give the swirl effect
 
+            # Only swirl the pixel if its less than the swirl radius
+            # r >= 0 as we are square rooting
+            if r <= swirl_radius:
+                # Use a linear scale for the swirl, pixels near the center have the most swirl
+                # swirl_direction can be used to switch between clockwise and anti-clockwise
+                theta += swirl_angle * (1 - r / swirl_radius) * swirl_direction
+
+            # Convert back to cartesian
+            new_x = round(math.cos(theta) * r) - centre_x
+            new_y = round(math.sin(theta) * r) - centre_y
+
+            if (new_x, new_y) in s:
+                print("!")
+
+            s.add((new_x, new_y))
+
+            # Map the pixels to the new image
+            new_img[x, y] = img[new_x, new_y]
 
     return new_img
 
-swirled = swirl_image(img)
-
-# print(to_polar_coordinates(1, 1), to_cartesian(*to_polar_coordinates(1, 1)))
-# print(to_polar_coordinates(-1, 1), to_cartesian(*to_polar_coordinates(-1, 1)))
-# print(to_polar_coordinates(-1, -1), to_cartesian(*to_polar_coordinates(-1, -1)))
-# print(to_polar_coordinates(1, -1), to_cartesian(*to_polar_coordinates(1, -1)))
-# print(to_polar_coordinates(0, -1), to_cartesian(*to_polar_coordinates(0, -1)))
-# print(to_polar_coordinates(0, 1), to_cartesian(*to_polar_coordinates(0, 1)))
-# print(to_polar_coordinates(-1, 0), to_cartesian(*to_polar_coordinates(-1, 0)))
-# print(to_polar_coordinates(1, 0), to_cartesian(*to_polar_coordinates(1, 0)))
+swirled = swirl_image(img, 150, math.pi / 2)
 
 concat = np.concatenate((img, swirled), axis=1)
 cv2.namedWindow("Displayed Image", cv2.WINDOW_NORMAL)
