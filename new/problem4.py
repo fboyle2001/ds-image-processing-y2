c6e369969f15aa436b932861a0ab262eeee308ae
fboyle2001 <finlayboyle2001@gmail.com>
2021-01-29 17:44:55 +0000

    LPBF with colour images

--- new/problem4.py
+++ new/problem4.py
@@ -54,38 +54,49 @@ def swirl_image(img, swirl_radius, swirl_angle, swirl_direction, bilinear=True):
 
     return new_img
 
-def low_pass_butterworth(img, n = 1, K = 15):
+def low_pass_butterworth(channel, n, K):
     # Fourier Transform of the image
-    fft = np.fft.fft2(img)
+    fft = np.fft.fft2(channel)
     # Shift it so (0, 0) is in the centre
     fft = np.fft.fftshift(fft)
-    img_width, img_height = img.shape
+    img_width, img_height = channel.shape
     # Calculate the coordinates of the centre
     centre_x, centre_y = img_width // 2, img_height // 2
 
     lpf = np.zeros((img_width, img_height), np.float64)
 
-    for x in range(-2 * K, 2 * K):
-        for y in range(-2 * K, 2 * K):
+    for x in range(-centre_x, centre_x):
+        for y in range(-centre_y, centre_y):
             dist = math.sqrt(x ** 2 + y ** 2)
             lpf[centre_x + x, centre_y + y] = 1 / (1 + np.power(dist / K, 2 * n))
-    #np.set_printoptions(threshold=sys.maxsize)
-    #print(lpf[centre_x-h:centre_x+h, centre_y-h:centre_y+h])
-    #lpf[centre_x-30:centre_x+30, centre_y-30:centre_y+30] = 1
+
+    # Convolution is the same as multiplication on freq domain
     fft *= lpf
 
+    # Reverse the centre shift
     f_ishift = np.fft.ifftshift(fft)
+    # Reverse the fourier transform
     ifft = np.fft.ifft2(f_ishift)
+    # Take abs as the values could be complex
     ifft = np.abs(ifft)
     return np.uint8(ifft)
 
-img = cv2.imread("face1.jpg", cv2.IMREAD_GRAYSCALE)
-lpf = low_pass_butterworth(img)
-# swirl_direction = -1
-# swirled = swirl_image(img, 170, math.pi / 2, swirl_direction, bilinear=True)
-# reversed = swirl_image(swirled, 170, math.pi / 2, -swirl_direction, bilinear=True)
-# diff = img - reversed
+def lpb_colour(img, n, K):
+    b, g, r = cv2.split(img)
 
-concat = np.concatenate((img, lpf), axis=1)
+    bl = low_pass_butterworth(b, n, K)
+    gl = low_pass_butterworth(g, n, K)
+    rl = low_pass_butterworth(r, n, K)
+
+    return cv2.merge((bl, gl, rl))
+
+img = cv2.imread("face1.jpg", cv2.IMREAD_COLOR)
+lpf = lpb_colour(img, 1, 40)
+swirl_direction = -1
+swirled = swirl_image(lpf, 170, math.pi / 2, swirl_direction, bilinear=True)
+reversed = swirl_image(swirled, 170, math.pi / 2, -swirl_direction, bilinear=True)
+diff = img - reversed
+
+concat = np.concatenate((img, lpf, swirled, reversed, diff), axis=1)
 cv2.imshow("Displayed Image", concat)
 cv2.waitKey(0)
